53994d4b0679667cfded57912b9f376c6993d12a
know <do.i.know.it@yggdrashill.com>
2020-11-29 20:44:25 +0900

    Add configuration for webpack

---
+++ assets/scripts/index.js
@@ -0,0 +1,3 @@
+const content = "Hello world in JavaScript";
+
+document.getElementById("root").innerHTML = content;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const rootPath = path.join(__dirname, "./");
+const inputPath = path.join(rootPath, "assets");
+const outputPath = path.join(rootPath, "build");
+
+module.exports =
+{
+    mode: "development",
+    entry: path.join(inputPath, "scripts", "index.js"),
+    output:
+    {
+        path: path.join(outputPath, "scripts"),
+        filename: "index.js",
+    },
+    plugins:
+    [
+        new HtmlWebpackPlugin
+        (
+            {
+                filename: "index.html",
+                template: path.join(inputPath, "scripts", "index.html"),
+            }
+        )
+    ],
+    devtool: "inline-source-map",
+    devServer:
+    { 
+        contentBase: path.join(outputPath, "scripts"),
+        port: 8080,
+        inline: true,
+        hot: true,
+        watchContentBase: true,
+    },
+};
