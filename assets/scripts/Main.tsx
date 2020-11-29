a21275af5762d9fe734aa3fa667f6ea68c45b6d5
know <do.i.know.it@yggdrashill.com>
2020-11-29 22:42:58 +0900

    Add main component for product

---
+++ assets/scripts/Main.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+
+const Main : FunctionComponent = () =>
+{
+    return <h1>Grambientation</h1>;
+}
+
+export { Main }
--- assets/scripts/index.tsx
+++ assets/scripts/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
+import { Main } from "./Main";
 
 const identifier = "root";
 const element = document.getElementById(identifier);
-const content = <main>Hello world in React</main>;
 
-render(content, element);
+render(<Main/>, element);
