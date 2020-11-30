3269d2422dce6ea768b7e19442034763e0a45def
know <do.i.know.it@yggdrashill.com>
2020-11-30 09:24:05 +0900

    Modify to describe header/main/footer

--- assets/scripts/Main.tsx
+++ assets/scripts/Main.tsx
@@ -2,7 +2,24 @@ import React, { FunctionComponent } from "react";
 
 const Main : FunctionComponent = () =>
 {
-    return <h1>Grambientation</h1>;
+    const [width, height] = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+    const [headerWidth, headerHeight] = [width, 60];
+    const [footerWidth, footerHeight] = [width, 60];
+    const [mainWidth, mainHeight] = [width, height - (headerHeight + footerHeight)];
+
+    return (
+        <div style={{ width: `${width}px`, height: `${height}px` }}>
+            <header style ={{ width: `${headerWidth}px`, height: `${headerHeight}px`, position: "relative", }}>
+                header
+            </header>
+            <main style={{ width: `${mainWidth}px`, height: `${mainHeight}px`, position: "relative", }}>
+                content
+            </main>
+            <footer style ={{ width: `${footerWidth}px`, height: `${footerHeight}px`, position: "relative", }}>
+                footer
+            </footer>
+        </div>
+    );
 }
 
 export { Main }
