0d16c980044785c5da4b5524bd7560119ac5682f
know <do.i.know.it@yggdrashill.com>
2020-11-30 09:46:09 +0900

    Modify to use Material-UI

--- assets/scripts/Main.tsx
+++ assets/scripts/Main.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { useRef, FunctionComponent } from "react";
+import { IconButton, Tooltip, Typography } from "@material-ui/core";
+import { GetApp } from "@material-ui/icons";
 
 const Main : FunctionComponent = () =>
 {
@@ -8,15 +10,21 @@ const Main : FunctionComponent = () =>
     const [mainWidth, mainHeight] = [width, height - (headerHeight + footerHeight)];
 
     return (
-        <div style={{ width: `${width}px`, height: `${height}px` }}>
+        <div style={{ width: `${width}px`, height: `${height}px`, }}>
             <header style ={{ width: `${headerWidth}px`, height: `${headerHeight}px`, position: "relative", }}>
-                header
+                <Typography component={"h1"} style={{ lineHeight: `${headerHeight}px`, fontSize: "2em", fontWeight: "bold", fontStyle: "italic", position: "absolute", top: "0px", left: "0px", margin: "0 10px", }}>
+                    Grambientation
+                </Typography>
             </header>
             <main style={{ width: `${mainWidth}px`, height: `${mainHeight}px`, position: "relative", }}>
                 content
             </main>
             <footer style ={{ width: `${footerWidth}px`, height: `${footerHeight}px`, position: "relative", }}>
-                footer
+                <Tooltip arrow={true} title="Download image" style={{ position: "absolute", bottom: "0px", right: "0px", }}>
+                    <IconButton aria-label="download image">
+                        <GetApp/>
+                    </IconButton>
+                </Tooltip>
             </footer>
         </div>
     );
